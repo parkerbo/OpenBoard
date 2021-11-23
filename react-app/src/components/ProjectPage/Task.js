40680b25374bb9247f332e5e9bf8a59fae315ff0
Parker Bolick <bolickparker@gmail.com>
2021-11-23 10:55:31 -0500

    Updated styling, added favicon

--- react-app/src/components/ProjectPage/Task.js
+++ react-app/src/components/ProjectPage/Task.js
@@ -3,13 +3,37 @@ import Container from "./Container";
 import { MdCheckCircleOutline } from "react-icons/md";
 import { useTaskDetail } from "../../context/TaskDetailContext";
 import { Draggable } from "react-beautiful-dnd";
-import { useState, useEffect } from "react";
+import { useRef, useState , useEffect} from "react";
 const Task = ({task, index}) => {
-
+    const taskRef = useRef();
+    const [active, setActive] = useState(false);
     const {setShowTaskDetail, setCurrentTask} = useTaskDetail();
-    const openTaskDetails = () => {
+    useEffect(() => {
+			// add when mounted
+			document.addEventListener("mousedown", handleClick);
+			// return function to be called when unmounted
+			return () => {
+				document.removeEventListener("mousedown", handleClick);
+			};
+		}, []);
+
+		const handleClick = (e) => {
+			if (taskRef.current.contains(e.target)) {
+				// inside click
+				return setActive(true);
+			}
+			return setActive(false);
+
+		};
+    const openTaskDetails = (e) => {
+        setActive(true)
         setShowTaskDetail(true);
-        setCurrentTask(task)
+        setCurrentTask(task);
+        setTimeout(() => {
+            e.target.scrollIntoView({behavior:'smooth'});
+
+        }, 200)
+
     }
 
 	return (
@@ -17,35 +41,36 @@ const Task = ({task, index}) => {
 			{(provided, snapshot) => (
 				<Container>
 					<div
-                        onClick={openTaskDetails}
+						onClick={openTaskDetails}
 						ref={provided.innerRef}
 						{...provided.draggableProps}
 						{...provided.dragHandleProps}
-						className={`task-card ${
+						className={`${active ? "task-card-active" : "task-card"} ${
 							snapshot.isDragging ? "task-dragging" : "task-drag-null"
 						}`}
 					>
-						{" "}
-						<div className="task-card-title">
-							<div id="task-card-check-mark">
-								<MdCheckCircleOutline size="1.2em" />
+						<div ref={taskRef}>
+							<div className="task-card-title">
+								<div id="task-card-check-mark">
+									<MdCheckCircleOutline size="1.2em" />
+								</div>
+								<h3>{task.title}</h3>
 							</div>
-							<h3>{task.title}</h3>
-						</div>
-						<div className="task-card-indicators">
-							{task.priority !== 'null' ? (
-								<div
-									className="task-card-indicator"
-									id={`priority-${task.priority}`}
-								>
-									{task.priority}
-								</div>
-							) : null}
-							{task.status !== 'null' ? (
-								<div className={`task-card-indicator status-${task.status}`}>
-									{task.status}
-								</div>
-							) : null}
+							<div className="task-card-indicators">
+								{task.priority !== "null" ? (
+									<div
+										className="task-card-indicator"
+										id={`priority-${task.priority}`}
+									>
+										{task.priority}
+									</div>
+								) : null}
+								{task.status !== "null" ? (
+									<div className={`task-card-indicator status-${task.status}`}>
+										{task.status}
+									</div>
+								) : null}
+							</div>
 						</div>
 					</div>
 				</Container>
